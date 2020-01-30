53924f9a5a1c7eb19e4d3c1921af93eff652d3e8
KovalenkoAV <merlinwt@gmail.com>
2020-01-30 16:34:29 +0300

    Session controller: add validation data for generate token
      - login: required|max:10
      - password: required|max:15

--- app/app/Http/Controllers/SessionController.php
+++ app/app/Http/Controllers/SessionController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SessionController extends Controller
 {
@@ -19,8 +20,18 @@ class SessionController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function generateToken(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'login' => 'required|max:10',
+            'password' => 'required|max:15',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors'=>$validator->errors()]);
+        }
+
         $login = $request->input('login');
         $password = $request->input('password');
+
         $token = '';
 
         if ($login == 'admin' && $password == 'root') {
